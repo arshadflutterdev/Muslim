06c64f08105b1cf13e11b6a2841d73c8730cde48
arshadflutterdev <arshadcodess@gmail.com>
2026-01-09 11:51:28 +0500

    changes

--- lib/Core/Screens/CodeToDownloadBooks/download_sahi-bukhari.dart
+++ lib/Core/Screens/CodeToDownloadBooks/download_sahi-bukhari.dart
@@ -1,7 +1,56 @@
-import 'dart:convert';
-import 'dart:io';
+// import 'dart:convert';
+// import 'dart:io';
 
-import 'package:Muslim/Core/Screens/MainScreens/AllAhaadees/HadeesinUrdu/SahiBukhari/hadith_details_model.dart';
+// import 'package:Muslim/Core/Screens/MainScreens/AllAhaadees/HadeesinUrdu/SahiBukhari/hadith_details_model.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:path_provider/path_provider.dart';
+
+// class DownloadSahiBukhar {
+//   final String apiKey =
+//       "%242y%2410%24pk5MeOVosBVG5x5EgPZQOuYdd4Mo6JFFrVOT2z9xGA9oAO4eu6rte";
+
+//   Future<void> downloadbook() async {
+//     try {
+//       final chapteresponse = await http.get(
+//         Uri.parse(
+//           "https://hadithapi.com/api/sahih-bukhari/chapters?apiKey=%242y%2410%24pk5MeOVosBVG5x5EgPZQOuYdd4Mo6JFFrVOT2z9xGA9oAO4eu6rte",
+//         ),
+//       );
+//       if (chapteresponse.statusCode != 200) {
+//         throw Exception("Failed to fetch Chapters");
+//       }
+//       final chapterJson = jsonDecode(chapteresponse.body);
+//       final chapterlist = chapterJson["chapters"] ?? [];
+
+//       //fetch hadith for all chapters
+//       for (var hadith in chapterlist) {
+//         final chapterNo = hadith["chapterNumber"];
+//         final hadithresponse = await http.get(
+//           Uri.parse(
+//             "https://hadithapi.com/api/hadiths/?book=sahih-bukhari&chapter=$chapterNo&apiKey=$apiKey",
+//           ),
+//         );
+//         if (hadithresponse.statusCode == 200) {
+//           final hadithdata = jsonDecode(hadithresponse.body);
+//           final hadithdetails = HadithDetails.fromJson(hadithdata);
+//           hadith["hadiths"] = hadithdetails.hadiths?.toJson();
+//         } else {
+//           print("failed to fetch all hadith data");
+//         }
+//       }
+//       //save all files to locally
+//       final dir = await getApplicationDocumentsDirectory();
+//       final file = File("${dir.path}/sahih-bukhari.json");
+
+//       await file.writeAsString(
+//         jsonEncode({"book": "sahih-bukhari", "chapters": chapterlist}),
+//       );
+//     } catch (e) {
+//       print("error ${e.toString()}");
+//     }
+//   }
+// }
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
@@ -10,43 +59,18 @@ class DownloadSahiBukhar {
       "%242y%2410%24pk5MeOVosBVG5x5EgPZQOuYdd4Mo6JFFrVOT2z9xGA9oAO4eu6rte";
 
   Future<void> downloadbook() async {
-    try {
-      final chapteresponse = await http.get(
-        Uri.parse(
-          "https://hadithapi.com/api/sahih-bukhari/chapters?apiKey=%242y%2410%24pk5MeOVosBVG5x5EgPZQOuYdd4Mo6JFFrVOT2z9xGA9oAO4eu6rte",
-        ),
-      );
-      if (chapteresponse.statusCode != 200) {
-        throw Exception("Failed to fetch Chapters");
-      }
-      final chapterJson = jsonDecode(chapteresponse.body);
-      final chapterlist = chapterJson["chapters"] ?? [];
+    final url =
+        "https://hadithapi.com/api/hadiths?book=sahih-bukhari&paginate=1000&apiKey=$apiKey";
 
-      //fetch hadith for all chapters
-      for (var hadith in chapterlist) {
-        final chapterNo = hadith["chapterNumber"];
-        final hadithresponse = await http.get(
-          Uri.parse(
-            "https://hadithapi.com/api/hadiths/?book=sahih-bukhari&chapter=$chapterNo&apiKey=$apiKey",
-          ),
-        );
-        if (hadithresponse.statusCode == 200) {
-          final hadithdata = jsonDecode(hadithresponse.body);
-          final hadithdetails = HadithDetails.fromJson(hadithdata);
-          hadith["hadiths"] = hadithdetails.hadiths?.toJson();
-        } else {
-          print("failed to fetch all hadith data");
-        }
-      }
-      //save all files to locally
-      final dir = await getApplicationDocumentsDirectory();
-      final file = File("${dir.path}/sahih-bukhari.json");
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File("${dir.path}/sahih-bukhari.json");
 
-      await file.writeAsString(
-        jsonEncode({"book": "sahih-bukhari", "chapters": chapterlist}),
-      );
-    } catch (e) {
-      print("error ${e.toString()}");
-    }
+    final request = http.Request('GET', Uri.parse(url));
+    final response = await http.Client().send(request);
+
+    final sink = file.openWrite();
+    await response.stream.pipe(sink);
+    await sink.flush();
+    await sink.close();
   }
 }
